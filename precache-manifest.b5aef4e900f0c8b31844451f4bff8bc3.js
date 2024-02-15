adc6ee2e09a7a6aab013cff3ad4f4e0ca43a062b
RutulPatel007 <116881681+RutulPatel007@users.noreply.github.com>
2024-02-15 21:55:36 +0530

    Updates

---
+++ precache-manifest.b5aef4e900f0c8b31844451f4bff8bc3.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8f36e591a8bb98e805c6941c96eef2",
+    "url": "/Personal-Portfolio/index.html"
+  },
+  {
+    "revision": "a912d2871220fe2676c1",
+    "url": "/Personal-Portfolio/static/css/2.0bc5d0a4.chunk.css"
+  },
+  {
+    "revision": "de58f4925a9f0c5fb46f",
+    "url": "/Personal-Portfolio/static/css/main.602c9943.chunk.css"
+  },
+  {
+    "revision": "a912d2871220fe2676c1",
+    "url": "/Personal-Portfolio/static/js/2.b87dc0b3.chunk.js"
+  },
+  {
+    "revision": "9d2ee682caf70354d592",
+    "url": "/Personal-Portfolio/static/js/3.bb311e20.chunk.js"
+  },
+  {
+    "revision": "de58f4925a9f0c5fb46f",
+    "url": "/Personal-Portfolio/static/js/main.f02c3aa8.chunk.js"
+  },
+  {
+    "revision": "79115b53a6de87605466",
+    "url": "/Personal-Portfolio/static/js/runtime~main.4195edd1.js"
+  },
+  {
+    "revision": "54a139e3f594d594c575d1e828af610e",
+    "url": "/Personal-Portfolio/static/media/1693190469944.54a139e3.jpg"
+  },
+  {
+    "revision": "50d9f1ce0d19cf942d3fe52c7268ea9a",
+    "url": "/Personal-Portfolio/static/media/3.50d9f1ce.jpg"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/Personal-Portfolio/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/Personal-Portfolio/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/Personal-Portfolio/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "271b78798f8bd16f7f9f986da66c4209",
+    "url": "/Personal-Portfolio/static/media/Screenshot (18).271b7879.png"
+  },
+  {
+    "revision": "f750170aaddf4e5f885a27d391a311d9",
+    "url": "/Personal-Portfolio/static/media/arrow1.f750170a.svg"
+  },
+  {
+    "revision": "9070490babe4d1aa7f077b104555b363",
+    "url": "/Personal-Portfolio/static/media/arrow2.9070490b.svg"
+  },
+  {
+    "revision": "5f3ca49706055342e92708c7dd31849f",
+    "url": "/Personal-Portfolio/static/media/banner-bg.5f3ca497.png"
+  },
+  {
+    "revision": "d64094774a77a02c2c22356681131cb8",
+    "url": "/Personal-Portfolio/static/media/color-sharp.d6409477.png"
+  },
+  {
+    "revision": "42c4dbf649597b4d4ceaf4c9792de60a",
+    "url": "/Personal-Portfolio/static/media/color-sharp2.42c4dbf6.png"
+  },
+  {
+    "revision": "a8822e204211be92b2412ccbe284e785",
+    "url": "/Personal-Portfolio/static/media/contact-img.a8822e20.svg"
+  },
+  {
+    "revision": "e9228c17c13a87653f1b8ed01a3ff306",
+    "url": "/Personal-Portfolio/static/media/footer-bg.e9228c17.png"
+  },
+  {
+    "revision": "0eab377e4d3dda5c15f498d77a4956d0",
+    "url": "/Personal-Portfolio/static/media/gettyimages-1311966784-612x612.0eab377e.jpg"
+  },
+  {
+    "revision": "83fbe3a610d01ea0d61a53499bcb73d3",
+    "url": "/Personal-Portfolio/static/media/google-photos.83fbe3a6.jpg"
+  },
+  {
+    "revision": "bfe6d1a6d579d73c0c625ec0afe2855b",
+    "url": "/Personal-Portfolio/static/media/header-img.bfe6d1a6.svg"
+  },
+  {
+    "revision": "5923348f2f03bbf3dcc1b453a31c2323",
+    "url": "/Personal-Portfolio/static/media/istockphoto-540983728-612x612.5923348f.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Personal-Portfolio/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "df35c0f11e12a3786e5453a4aa501337",
+    "url": "/Personal-Portfolio/static/media/logo_sub-removebg-preview.df35c0f1.png"
+  },
+  {
+    "revision": "dea05c56b45857a24dcba5a23202ec70",
+    "url": "/Personal-Portfolio/static/media/meter1.dea05c56.svg"
+  },
+  {
+    "revision": "7fb1b05f49ba5c3b4570e1dec3728213",
+    "url": "/Personal-Portfolio/static/media/meter2.7fb1b05f.svg"
+  },
+  {
+    "revision": "19e4204daee7b06ccd0aa7807bc37653",
+    "url": "/Personal-Portfolio/static/media/meter3.19e4204d.svg"
+  },
+  {
+    "revision": "dff2b6246274897c980cd8a354f7f979",
+    "url": "/Personal-Portfolio/static/media/nav-icon1.dff2b624.svg"
+  },
+  {
+    "revision": "0393a6c85ddc105c9304f0f7787ddaea",
+    "url": "/Personal-Portfolio/static/media/nav-icon2.0393a6c8.svg"
+  },
+  {
+    "revision": "81ff4e970a39053a0acdc10fd58dc5c4",
+    "url": "/Personal-Portfolio/static/media/nav-icon3.81ff4e97.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/Personal-Portfolio/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/Personal-Portfolio/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/Personal-Portfolio/static/media/revicons.2feb69cc.eot"
+  }
+]);
